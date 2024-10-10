e03e1dc44bdc1c566fd5f29983a86bd9699acce7
unknown <santiago.nicolas.gomez@estudiante.epsum.school>
2024-10-10 14:04:48 +0200

    ejercicio Alumnos

---
+++ src/epsum/clasescamilo/ejAlumnos/main/importAlumnosMain.java
@@ -0,0 +1,22 @@
+package epsum.clasescamilo.ejAlumnos.main;
+
+import epsum.clasescamilo.ejAlumnos.clases.Alumno;
+
+import java.util.ArrayList;
+
+public class importAlumnosMain {
+    public static void main(String[] args) {
+        ArrayList<Alumno> alumnos = new ArrayList<>();
+        alumnos.add(new Alumno("Elena",19,"dam",2,9 ));
+        alumnos.add(new Alumno("Juan",18,"daw",1,7 ));
+        alumnos.add(new Alumno("Eva",21,"Marketing",1,8 ));
+        alumnos.add(new Alumno("Francisco",19,"dam",2,6 ));
+
+
+
+
+
+
+
+    }
+}
